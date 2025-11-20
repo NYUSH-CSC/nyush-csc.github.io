bd87862fa02d36936939bebf887616ec7e1c298d
Mandy3512 <mq2235@nyu.edu>
2025-11-20 20:21:00 +0800

    about team

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## About this page
 This is a website for the Cybersecurity Club at NYU Shanghai. 
+The webpage is "About Team" with 2 functinons.
+One search for a member already exist, and then send a message to this person's email.
+The other add a new member to the team and update the display of the team members and their role.
--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-
+const members = {
+    "A":"aaa@sendaemail.com",
+    "B":"bbbb@sendaemail.com",
+    "C":"ccccc@sendaemail.com",
+    "D":"dd@sendaemail.com",
+    "E":"e@sendaemail.com"};
+document.getElementById("Search Form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = document.getElementById("searchName").value.trim();
+    const result= document.getElementById("searchResult");
+    const contact= document.getElementById("contactsection");
+    const addNewMember= document.getElementById("AddNewMember");
+    contact.style.display = "none";
+    addNewMember.style.display = "none";
+    if (members[name]) {
+        result.innerHTML = "Team Member Found";
+        contact.style.display = "block";
+    }
+    else {
+        result.innerHTML = "No Team Member Found. Would you like to add "+name+" to the Team?";
+        const newname = document.getElementById("Name");
+        newname.innerHTML="Name: "+name;
+        addNewMember.style.display = "block";
+    }
+});
+document.getElementById("Contact Form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = document.getElementById("searchName").value.trim();
+    const subject = document.getElementById("contactsubject").value;
+    const message = document.getElementById("contactmessage").value;
+    const result= document.getElementById("searchResult");
+    alert("Message sent to " + name+" ("+members[name] +")"+ " with subject: " + subject);
+    document.getElementById("contactsubject").value = "";
+    document.getElementById("contactmessage").value = "";
+    document.getElementById("searchName").value = "";
+    result.innerHTML = "";
+    document.getElementById("contactsection").style.display = "none";
+});
+document.getElementById("Add Member Form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const addNewMember= document.getElementById("AddNewMember");
+    const name = document.getElementById("searchName").value.trim();
+    const role = document.getElementById("newmemberrole").value.trim();
+    let email = document.getElementById("newmemberemail").value.trim();
+    if (!email.endsWith("@sendaemail.com")) {
+        const userconfirm=window.confirm("Please enter a valid email ending with @sendaemail.com, would you like me to add this for you");
+        if (!userconfirm){
+            return;
+        }
+        else {
+            email = email + "@sendaemail.com";
+        }
+    }
+    const result= document.getElementById("searchResult");
+    alert(name + " has been added to the Team with role: " + role);
+    document.getElementById("teamlist").innerHTML +=`<div class="member">${name} - ${role}</div>`;
+    members[name]=email;
+    document.getElementById("newmemberrole").value="";
+    document.getElementById("newmemberemail").value="";
+    document.getElementById("searchName").value = "";
+    result.innerHTML = "";
+    addNewMember.style.display = "none";
+});
